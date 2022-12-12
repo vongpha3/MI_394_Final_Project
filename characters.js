a939f64e1977f21cd9306d799880e0bcbb068ece
vongpha3 <vongpha3@msu.edu>
2022-12-11 21:50:23 -0500

    Project finished

--- characters.js
+++ characters.js
@@ -1,11 +1,11 @@
 let char_list = [
 
     {id: "natasha", name: "Natasha Grazun", flavor: "The Girl in Question", img: "images/Test_Full_3.png", 
-        desc: "Natasha (aka The Rat) is more comparable to a clever mouse: quiet, cunning and always inquisitive. She has a habit of making your business her business and using her wit to solve them. Many are disarmed by her small and messy appearance, and all are in debate if the look is deliberate or from disinterest in such things."},
+        desc: "Natasha (aka The Rat) is more comparable to a clever mouse: quiet, cunning, and always inquisitive. She has a habit of making your business her business and using her wit to solve them. Many are disarmed by her small and messy appearance, and all are in debate if the look is deliberate or from disinterest."},
     {id: "laurna", name: "Laurna Hase", flavor: "Bewildered Butcher", img: "images/Test_Full_2.png", 
-        desc: ""},
-    {id: "laputa", name: "Laputa Lanhki", flavor: "The Scheming Snake", img: "images/Test_Full.png", desc: ""},
-    //{id: 2, name: "", flavor: "", img: "", desc: ""},
+        desc: "The apprentice butcher at The Boar's Den, Laurna's cleaving is as swift as her feet. Despite being timid as a rabbit, when the chips are down beware of her viewing you as her prey. One swing of her mighty custom cleaver will turn you into a fine meal for one of her customers."},
+    {id: "laputa", name: "Laputa Lanhki", flavor: "The Scheming Snake", img: "images/Test_Full.png", 
+        desc: "A woman of business and murder. You'll find her bartending at the Water Serpent nightclub if you're lucky enough to not have been turned to stone by her trusty bat \"Lil' Maddy\". The mystery of her hair seems to be one not even Natasha wants to investigate."},
 
 ]
 
@@ -32,6 +32,7 @@ function highlight(character) {
             image.src = char_list[i].img;
             children[0].textContent = char_list[i].name;
             children[1].textContent = char_list[i].flavor;
+            children[2].textContent = char_list[i].desc;
             continue;
 
         }
@@ -68,5 +69,11 @@ function arrowClick(direction) {
 
 
     highlight(char_list[index].id);
+
+}
+
+function moveTo() {
+
+    location.href = "characters.html";
 
 }
